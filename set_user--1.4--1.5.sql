66581e3e4e179fd0d5afaf70e1f70b19d8aa67db
Mike Palmiotto <mike.palmiotto@crunchydata.com>
2018-04-19 15:21:39 -0400

    Bump version to 1.5 to tag

---
+++ set_user--1.4--1.5.sql
@@ -0,0 +1,6 @@
+/* set-user-1.4--1.5.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION set_user UPDATE to '1.5'" to load this file. \quit
+
+-- just bumping our version to 1.5. no new sql function features here, so nothing to do.
