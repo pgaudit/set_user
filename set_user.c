pgaudit/set_user

commit 11788c71e39ad18be122c80ecc9cb974d1fe1bd1

Add group role support to superuser whitelist
This feature adds support for '+<group_role>' syntax in the superuser
whitelist. This should allow users to be added to the whitelist by
association with the specified "<group_role>".

Resolves: #7

set_user.c
@@ -93,6 +93,7 @@
 #include "catalog/pg_proc.h"
 #include "miscadmin.h"
 #include "tcop/utility.h"
+#include "utils/acl.h"
 #include "utils/builtins.h"
 #include "utils/guc.h"
 #include "utils/memutils.h"
@@ -208,9 +209,21 @@ check_user_whitelist(Oid userId, const char *whitelist)
 	{
 		char	   *elem = (char *) lfirst(l);
 
-		if (pg_strcasecmp(elem, GETUSERNAMEFROMID(userId)) == 0)
-			result = true;
-		else if (pg_strcasecmp(elem, WHITELIST_WILDCARD) == 0)
+		if (elem[0] == '+')
+		{
+			Oid roleId;
+			roleId = get_role_oid(elem + 1, false);
+			if (!OidIsValid(roleId))
+				result = false;
+
+			/* Check to see if userId is contained by group role in whitelist */
+			result = has_privs_of_role(userId, roleId);
+		}
+		else
+		{
+			if (pg_strcasecmp(elem, GETUSERNAMEFROMID(userId)) == 0)
+				result = true;
+			else if(pg_strcasecmp(elem, WHITELIST_WILDCARD) == 0)
 				/* No explicit usernames intermingled with wildcard. */
 				ereport(ERROR,
 						(errcode(ERRCODE_SYNTAX_ERROR),
@@ -219,8 +232,9 @@ check_user_whitelist(Oid userId, const char *whitelist)
 								 "or remove the wildcard character \"%s\". The whitelist "
 								 "cannot contain both.",
 								 WHITELIST_WILDCARD)));
+				result = true;
+		}
 	}
-
 	return result;
 }
 

commit ac837c32e9d290c77b5ded2c584515ed9bc5e956

Fix set_user merge error
Commit 11788c7 introduced a bug wherein all
`set_user.superuser_whitelist` entries were considered valid. This was
introduced by a bad merge. Revert the bad line to get this working
again.

set_user.c
@@ -234,7 +234,6 @@ check_user_whitelist(Oid userId, const char *whitelist)
 								 "or remove the wildcard character \"%s\". The whitelist "
 								 "cannot contain both.",
 								 WHITELIST_WILDCARD)));
-				result = true;
 		}
 	}
 	return result;