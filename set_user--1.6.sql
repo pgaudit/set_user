eb3041f74795ba8c9590ae5c1e62a8a8ce153409
Mike Palmiotto <mike.palmiotto@crunchydata.com>
2020-06-01 14:56:41 -0400

    Make set_user non-relocatable
    Qualify all set_user functions so they're installed to the extension
    install schema.

--- set_user--1.6.sql
+++ set_user--1.6.sql
@@ -1,42 +1,44 @@
 /* set-user--1.6.sql */
 
+SET LOCAL search_path to @extschema@;
+
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION set_user" to load this file. \quit
 
-CREATE FUNCTION set_user(text)
+CREATE FUNCTION @extschema@.set_user(text)
 RETURNS text
 AS 'MODULE_PATHNAME', 'set_user'
 LANGUAGE C;
 
-CREATE FUNCTION set_user(text, text)
+CREATE FUNCTION @extschema@.set_user(text, text)
 RETURNS text
 AS 'MODULE_PATHNAME', 'set_user'
 LANGUAGE C STRICT;
 
-REVOKE EXECUTE ON FUNCTION set_user(text) FROM PUBLIC;
-REVOKE EXECUTE ON FUNCTION set_user(text, text) FROM PUBLIC;
+REVOKE EXECUTE ON FUNCTION @extschema@.set_user(text) FROM PUBLIC;
+REVOKE EXECUTE ON FUNCTION @extschema@.set_user(text, text) FROM PUBLIC;
 
-CREATE FUNCTION reset_user()
+CREATE FUNCTION @extschema@.reset_user()
 RETURNS text
 AS 'MODULE_PATHNAME', 'set_user'
 LANGUAGE C;
 
-CREATE FUNCTION reset_user(text)
+CREATE FUNCTION @extschema@.reset_user(text)
 RETURNS text
 AS 'MODULE_PATHNAME', 'set_user'
 LANGUAGE C STRICT;
 
-GRANT EXECUTE ON FUNCTION reset_user() TO PUBLIC;
-GRANT EXECUTE ON FUNCTION reset_user(text) TO PUBLIC;
+GRANT EXECUTE ON FUNCTION @extschema@.reset_user() TO PUBLIC;
+GRANT EXECUTE ON FUNCTION @extschema@.reset_user(text) TO PUBLIC;
 
 /* New functions in 1.1 (now 1.4) begin here */
 
-CREATE FUNCTION set_user_u(text)
+CREATE FUNCTION @extschema@.set_user_u(text)
 RETURNS text
 AS 'MODULE_PATHNAME', 'set_user'
 LANGUAGE C STRICT;
 
-REVOKE EXECUTE ON FUNCTION set_user_u(text) FROM PUBLIC;
+REVOKE EXECUTE ON FUNCTION @extschema@.set_user_u(text) FROM PUBLIC;
 
 /* No new sql functions for 1.5 */
 /* No new sql functions for 1.6 */
