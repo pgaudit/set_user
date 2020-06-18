0ebaad04caad5c98958acd7629feadf2aac4e9c1
Mike Palmiotto <mike.palmiotto@crunchydata.com>
2020-06-18 11:18:50 -0400

    Prepare for v2.0.0 release
    - Get rid of TODO section in README
    - Add NOTES section in README for v2.0.0
    - Bump default version
    - Update upgrade path

---
+++ set_user--1.6--2.0.sql
@@ -0,0 +1,6 @@
+/* set-user-1.6--2.0.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION set_user UPDATE to '2.0'" to load this file. \quit
+
+-- just bumping our version to 2.0. no new sql function features here, so nothing to do.
