3a2048e00bc2888ccf12249abf9c6fbf39794d1c
Mike Palmiotto <mike.palmiotto@crunchydata.com>
2021-06-02 15:08:22 +0000

    Fix deprecated GUC handling
    Update deprecated GUC macros to follow the guidelines in
    src/backend/utils/misc/README (re: check_hook):
    
     > If the function wishes to replace a string value, it must malloc (not
     palloc) the replacement value, and be sure to free() the previous
     value.
    
    In this case, the free() is carried out by `set_string_field`, so it is
    unnecessary in the check_hook. A free in the check_hook results in an
    invalid free when running under valgrind.
    
    Create tighter constraints on the deprecation NOTICEs, such that they're
    only displayed for non-default deprecated GUC settings and only once.
    
    Remove GUCs from `show all` by using `GUC_NO_SHOW_ALL`. Previously we were
    unable to filter out the deprecated GUC, since the show_hook was hit during
    autocompletion, even when the GUC was marked with the no show flag.
    
    Update the set_user README to reflect changes.
    
    This commit should address pgaudit/set_user#32.

---
+++ deprecated_gucs.h
@@ -0,0 +1,85 @@
+/* -------------------------------------------------------------------------
+ *
+ * deprecated_gucs.h
+ *
+ * Definitions for deprecated set_user GUCs.
+ *
+ * Copyright (c) 2010-2021, PostgreSQL Global Development Group
+ *
+ * -------------------------------------------------------------------------
+ */
+
+#ifndef DEPRECATED_GUCS_H
+#define DEPRECATED_GUCS_H
+
+/*
+ * PostgreSQL GUC variable deprecation handling.
+ */
+#include "miscadmin.h"
+#include "utils/guc.h"
+#include "utils/varlena.h"
+
+static bool
+check_set_user_list(char **newval, void **extra, GucSource source,
+			const char *depname, const char *newname, bool *notice)
+{
+	/*
+	 * Notify on the first non-default change for the Postmaster PID only.
+	 */
+	if (MyProcPid == PostmasterPid && source != PGC_S_DEFAULT && *notice)
+	{
+		ereport(NOTICE, (errcode(ERRCODE_WARNING_DEPRECATED_FEATURE),
+				 errmsg("deprecated GUC: set_user.%s", depname),
+				 errhint("Use set_user.%s instead.", newname)));
+		*notice = false;
+	}
+
+	/*
+	 * If the deprecated value is being set, allocate some memory to copy
+	 * it to the new GUC, so they remain in sync. Do not free previous
+	 * allocated value here, as set_string_field will handle that. A call
+	 * to free here would only result in an invalid free in set_string_field.
+	 */
+	if (*newval)
+	{
+		*extra = strdup(*newval);
+		if (*extra == NULL)
+		{
+			ereport(FATAL,
+					(errcode(ERRCODE_OUT_OF_MEMORY),
+					 errmsg("out of memory")));
+		}
+	}
+
+	return true;
+}
+
+#define DefineDeprecatedStringVariable(deprecated_name, new_name, old_value, new_value) \
+	DefineCustomStringVariable("set_user." #deprecated_name, \
+				   "Deprecated: see \"set_user." #new_name "\"", \
+				   NULL, &old_value, ALLOWLIST_WILDCARD, PGC_SIGHUP, GUC_NO_SHOW_ALL, \
+				   check_ ##deprecated_name, assign_ ##deprecated_name, show_ ##deprecated_name)
+
+
+#define DEPRECATED_GUC(deprecated_name, new_name, old_value, new_value) \
+static char * old_value; \
+static bool notice_ ##old_value = true; \
+static bool \
+check_ ##deprecated_name(char **newval, void **extra, GucSource source) \
+{ \
+	return check_set_user_list(newval, extra, source, #deprecated_name, #new_name, &notice_ ##old_value); \
+} \
+static void \
+assign_ ##deprecated_name (const char *newval, void *extra) \
+{ \
+	if (extra) \
+	{ \
+		new_value = extra; \
+	} \
+} \
+static const char *show_ ##deprecated_name (void) \
+{ \
+	return new_value; \
+}
+
+#endif /* DEPRECATED_GUCS_H */
