pgaudit/set_user

commit b74c1d8ff01d10cdf027fb51a40c6f782613d064

Add additional conditional for set_session_auth()
If PG is built with cassert, NO_ASSERT_AUTH_UID_ONCE must be defined in
order to not bail on calls to set_session_auth().

set_user.c
@@ -551,15 +551,16 @@ PG_FUNCTION_INFO_V1(set_session_auth);
 Datum
 set_session_auth(PG_FUNCTION_ARGS)
 {
+#if defined(NO_ASSERT_AUTH_UID_ONCE)
 	char		   *newuser = text_to_cstring(PG_GETARG_TEXT_PP(0));
 	HeapTuple		roleTup;
 	bool			NewUser_is_superuser = false;
-
+#endif
 	if (exit_on_error)
 		/* We want to hard exit on *any* ERROR */
 		ExitOnAnyError = true;
 
-#ifdef USE_ASSERT_CHECKING
+#if defined(USE_ASSERT_CHECKING) && !defined(NO_ASSERT_AUTH_UID_ONCE)
 	elog(ERROR, "Assert build disables set_session_auth()");
 #else
 	/* Look up the username */

commit 39bfe6ba513ea4fc35675ea55615ebf52c820a16

Update authorized uid assertion logic
pgaudit/set_user@b74c1d8 introduced a preprocessor flag to disable
asserts on AuthorizedUserId initialization, but missed the case where
assertions are off altogether and no convencience flag is set.

This commit fixes that issue by simplifying the logic a bit and cleaning
up some of the associated code.

compatibility.h
@@ -12,6 +12,10 @@
 #ifndef SET_USER_COMPAT_H
 #define SET_USER_COMPAT_H
 
+#ifndef NO_ASSERT_AUTH_UID_ONCE
+#define NO_ASSERT_AUTH_UID_ONCE !USE_ASSERT_CHECKING
+#endif
+
 /*
  * PostgreSQL version 13+
  *

set_user.c
@@ -551,21 +551,14 @@ PG_FUNCTION_INFO_V1(set_session_auth);
 Datum
 set_session_auth(PG_FUNCTION_ARGS)
 {
-#if defined(NO_ASSERT_AUTH_UID_ONCE)
+	bool orig_exit_on_err = ExitOnAnyError;
+#if NO_ASSERT_AUTH_UID_ONCE
 	char		   *newuser = text_to_cstring(PG_GETARG_TEXT_PP(0));
 	HeapTuple		roleTup;
 	bool			NewUser_is_superuser = false;
-#endif
 
-	bool orig_exit_on_err = ExitOnAnyError;
+	ExitOnAnyError = exit_on_error;
 
-	if (exit_on_error)
-		/* We want to hard exit on *any* ERROR */
-		ExitOnAnyError = true;
-
-#if defined(USE_ASSERT_CHECKING) && !defined(NO_ASSERT_AUTH_UID_ONCE)
-	elog(ERROR, "Assert build disables set_session_auth()");
-#else
 	/* Look up the username */
 	roleTup = SearchSysCache1(AUTHNAME, PointerGetDatum(newuser));
 	if (!HeapTupleIsValid(roleTup))
@@ -582,6 +575,9 @@ set_session_auth(PG_FUNCTION_ARGS)
 				 errhint("Use \'set_user_u\' to escalate.")));
 
 	InitializeSessionUserId(newuser, InvalidOid);
+#else
+	ExitOnAnyError = exit_on_error;
+	elog(ERROR, "Assert build disables set_session_auth()");
 #endif
 
 	ExitOnAnyError = orig_exit_on_err;