7515737f683d54adf6ae59b9bf2352bd47b3f7fa
Keith Fiske <keith@keithf4.com>
2022-10-20 09:11:43 -0400

    Add files for 3.1 version upgrade

---
+++ set_user--3.0--3.1.sql
@@ -0,0 +1,6 @@
+/* set-user-3.0--3.1.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION set_user UPDATE" to load this file. \quit
+
+-- just bumping our version to 3.1. no new SQL features here, so nothing to do.
