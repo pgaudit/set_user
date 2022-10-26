0fdd321e35b079f46df61b8c1013d67e294d9958
Keith Fiske <keith@keithf4.com>
2022-10-26 12:02:56 -0400

    Add support for upgrading from 3.0 or 4.0.0RC1 to 4.0.0

---
+++ set_user--3.0--4.0.0.sql
@@ -0,0 +1,6 @@
+/* set-user-3.0--4.0.0.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION set_user UPDATE" to load this file. \quit
+
+-- just bumping our version to 4.0.0. no new SQL features here, so nothing to do.
